4af7a84efad42398c40b749f7330339e5e7959b8
Gaurab Gurung <gaurabgurung181@gmail.com>
2024-01-06 01:32:44 +1100

    added most of the features in the landing page of the app

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import HeroImg from "../../assets/hero/hero-1.png";
+import styles from "./Hero.module.css";
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Max</h1>
+        <p className={styles.description}>
+          I am a front-end developer. I have done my masters in Computer
+          Science. Reach out if you'd like to learn more!
+        </p>
+        <a
+          href="mailto:gaurabgurung181@gmail.com"
+          className={styles.containerBtn}
+        >
+          Contact Me
+        </a>
+      </div>
+      <img src={HeroImg} alt="Hero image" className={styles.heroImg} />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 
 import styles from "./nav-bar.module.css";
-import { getImageUrl } from "../../utils";
-import menuIcon from "../../assets/nav/menuIcon.png";
+import MenuIcon from "../../assets/nav/menuIcon.png";
+import CloseIcon from "../../assets/nav/closeIcon.png";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
         Portfolio
       </a>
       <div className={styles.menu}>
-        <img className={styles.menuBtn} src={menuIcon} alt="menu-button" />
-        <ul className={styles.menuItems}>
+        <img
+          className={styles.menuBtn}
+          src={menuOpen ? CloseIcon : MenuIcon}
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
           <li>
             <a href="#about">About</a>
           </li>
